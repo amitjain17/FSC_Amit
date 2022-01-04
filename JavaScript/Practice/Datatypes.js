6a6e44e5761bfb2473ee7563424133b8376f92d8
Amit Jain <amitjainpwr@gmail.com>
2022-01-04 16:21:47 +0530

    javascript datatypes file added

---
+++ JavaScript/Practice/Datatypes.js
@@ -0,0 +1,13 @@
+//automatic integer value will come out from String
+let str1 = "123 Amit"
+console.log(parseInt(str1) ,typeof str1)
+
+//automatic string value change to Interger
+let num="6"
+let num2 = 2 - num
+console.log(num2, typeof num2)
+
+//changing datatype of string to Number
+let num3 = '2'
+num3 = Number(num3)
+console.log(num3, typeof num3)
