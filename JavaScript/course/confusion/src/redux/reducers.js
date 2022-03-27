f575f166d4e5c664a7e8c70eb4b3edd8fb9f248e
Amit Jain <amitjainpwr@gmail.com>
2022-03-27 20:51:18 +0530

    combine reducer add in redux

--- JavaScript/course/confusion/src/redux/reducers.js
+++ JavaScript/course/confusion/src/redux/reducers.js
@@ -1,16 +1,16 @@
-import { COMMENTS } from './../shared/comments.js';
-import { PROMOTIONS } from './../shared/promotions.js';
-import { LEADERS } from './../shared/leaders.js';
-import { DISHES } from '../shared/menuData.js';
+// import { COMMENTS } from './../shared/comments.js';
+// import { PROMOTIONS } from './../shared/promotions.js';
+// import { LEADERS } from './../shared/leaders.js';
+// import { DISHES } from '../shared/menuData.js';
 
-export const initialState = {
-    selectedDish: 0,
-    dishes: DISHES,
-    comments: COMMENTS,
-    promotions: PROMOTIONS,
-    leaders: LEADERS
-}
+// export const initialState = {
+//     selectedDish: 0,
+//     dishes: DISHES,
+//     comments: COMMENTS,
+//     promotions: PROMOTIONS,
+//     leaders: LEADERS
+// }
 
-export const Reducer = (state = initialState, action) => {
-    return state;
-}
+// export const Reducer = (state = initialState, action) => {
+//     return state;
+// }
